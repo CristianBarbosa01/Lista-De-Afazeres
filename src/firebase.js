cabf118238894f06d4a43eb6b0885ca972136193
CristianBarbosa01 <an.cristianbarbosa.an@gmail.com>
2021-08-04 16:09:54 -0300

    Add: cadastrar UID do usuario

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 var firebaseConfig = {
   apiKey: "AIzaSyANeVM-tDxTtsHjoLU7D9TssnXiNZfnZos",
   authDomain: "lista-de-tarefas-9ce49.firebaseapp.com",
-  databaseURL: "https://lista-de-tarefas-9ce49-default-rtdb.firebaseio.com",
+  databaseURL: "https://lista-de-tarefas-9ce49-default-rtdb.firebaseio.com/",
   projectId: "lista-de-tarefas-9ce49",
   storageBucket: "lista-de-tarefas-9ce49.appspot.com",
-  messagingSenderId: "G-ET94J7JV7T"
+  messagingSenderId: "G-ET94J7JV7T",
 };
 
-const firebaseApp = firebase.initializeApp (firebaseConfig);
-const db = firebase.firestore();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-export { auth };
-export default db;
+const database = firebase.database();
+export { auth, database };
