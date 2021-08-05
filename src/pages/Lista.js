c3ba4fbcff76feb03098e07792fd2e458dfe74fa
CristianBarbosa01 <an.cristianbarbosa.an@gmail.com>
2021-08-05 15:48:14 -0300

    Mostrar os dados do firebase no layout

--- src/pages/Lista.js
+++ src/pages/Lista.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { database } from "../firebase";
 import Modal from "../components/Modal";
 import "../components/Modal.css";
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [tarefas, setTarefas] = useState([]);
+  const tarefasRef = database.ref("tarefas");
+  const uid = sessionStorage.getItem("uid");
+
+  useEffect(() => {
+    let tasks = [];
+    tarefasRef.child(uid).once("value", (data) => {
+      const valores = data?.val();
+      Object.keys(valores).map((key) => {
+        tasks.push(valores[key]);
+      });
+      setTarefas(tasks);
+    });
+  }, []);
+
+  console.log(tarefas);
 
   return (
     <div className="uHome">
@@ -16,9 +32,29 @@ const Home = () => {
           setOpenModal(true);
         }}
       >
-        <strong style={{ fontSize: 30, }}>+</strong>
+        <strong style={{ fontSize: 30 }}>+</strong>
       </button>
       {openModal && <Modal closeModal={setOpenModal} />}
+      {tarefas?.length > 0 && (
+        <div style={{ overflow: "auto" }}>
+          {tarefas?.map((t, key) => (
+            <div
+              key={key}
+              style={{
+                width: 500,
+                minHeight: 100,
+                borderRadius: 15,
+                backgroundColor: "#f9f9f9",
+                margin: 15,
+                padding: 10,
+              }}
+            >
+              <p>{`Data: ${t?.data?.replaceAll("-", "/")}`}</p>
+              <p>{`Descricao: ${t?.descricao}`}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
